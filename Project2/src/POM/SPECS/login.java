1d2047b4a43511b74ceedeb5e4d748f3324762cf
ayan.pal <ayan.pal@exponential.com>
2020-05-31 23:24:39 +0530

    Demo Commit 3

--- Project2/src/POM/SPECS/login.java
+++ Project2/src/POM/SPECS/login.java
@@ -8,18 +8,22 @@ import java.io.File;
 import org.apache.commons.io.FilenameUtils;
 import java.io.File;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.*;
 
 public class login {
 
-	public static void login1() throws IOException {
+	public static void login1() throws IOException, InterruptedException {
 		POM.COMMON.common.openBrowser();
 		
 		  POM.CONFIGURATION.driver.driver.get(POM.CONFIGURATION.Configuration.siteurl);
 		  
 		  POM.PAGES.webpage.getSearchbox().sendKeys(POM.PAGES.webpage.search_item); 
 		  POM.PAGES.webpage.getBtn_search().click();
+		  
+		  WebDriverWait wait1 = new WebDriverWait(POM.CONFIGURATION.driver.driver , 30);
+		  wait1.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Men's Gb 2354116_Olive Green_5 Boots"))) ;
 		  
 		  File scrFile;
 	        scrFile = ((TakesScreenshot)POM.CONFIGURATION.driver.driver).getScreenshotAs(OutputType.FILE); 
@@ -36,6 +40,9 @@ public class login {
 	        
 	        
             POM.PAGES.webpage.getBtn_addtocart().click();
+            
+            WebDriverWait wait2 = new WebDriverWait(POM.CONFIGURATION.driver.driver , 30);
+  		  wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div/div/div/h1"))) ;
 	        
 	        File scrFile2;
 	        scrFile2 = ((TakesScreenshot)POM.CONFIGURATION.driver.driver).getScreenshotAs(OutputType.FILE); 
